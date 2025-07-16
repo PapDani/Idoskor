0d20bda358f6f23a2d24ed3d4a399153747aeb34
PapDani <papdaniel9977@gmail.com>
2025-07-16 16:47:17 +0200

    feat: Angular Material added to project & simple material header and card added

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
+import { MaterialModule } from './material.module';
+import { SpinnerComponent } from './shared/spinner/spinner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet],
+  imports: [HeaderComponent, RouterOutlet,
+    MaterialModule, SpinnerComponent],
   template: `
+    <app-spinner></app-spinner>
     <app-header></app-header>
     <router-outlet></router-outlet>
   `
--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -4,15 +4,18 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@a
 
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
-
 import { Configuration } from './app/api/configuration';
 import { JwtInterceptor } from './app/interceptors/jwt.interceptor';
+import { LoadingInterceptor } from './app/interceptors/loading.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
+    provideAnimations(),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
     // Override basePath so the client calls “/api” on port 4200:
     {
       provide: Configuration,
