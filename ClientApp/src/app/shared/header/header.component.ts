250464bb0a332ab5df757fad60a916bd8ac5afea
Pap Dániel <43150057+PapDani@users.noreply.github.com>
2025-08-11 15:24:05 +0200

    feat: simple, basic material menu added (#27)

--- ClientApp/src/app/shared/header/header.component.ts
+++ ClientApp/src/app/shared/header/header.component.ts
@@ -5,29 +5,21 @@ import { AuthService } from '../../services/auth.service';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatToolbarModule, MatButtonModule, MatIconModule, MatButtonModule],
-  template: `
-    <mat-toolbar color="primary">
-      <span class="cursor-pointer" (click)="goHome()">Időskor</span>
-      <span class="spacer"></span>
-
-      <ng-container *ngIf="!auth.isAdmin">
-        <button mat-button routerLink="/cards">Home</button>
-        <button mat-button routerLink="/login">Login</button>
-      </ng-container>
-
-      <ng-container *ngIf="auth.isAdmin">
-        <button mat-button routerLink="/admin/cards">Edit cards</button>
-        <button mat-button routerLink="/admin/cards/new">New card</button>
-        <button mat-button (click)="logout()">Logout</button>
-      </ng-container>
-    </mat-toolbar>
-  `
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule
+  ],
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
   constructor(public auth: AuthService, private router: Router) { }
